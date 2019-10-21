d18dcfe1b56680404d1768a17269205d0d832d64
cherrylim99 <kokmen818@hotmail.com>
2019-10-21 12:28:08 +0800

    Practice_1_RollDice_Updated

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -19,15 +19,23 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton2: Button = findViewById(R.id.roll_button2)
         rollButton2.setOnClickListener { rollDice2() }
+
+        val rollButton3: Button = findViewById(R.id.roll_button3)
+        rollButton3.setOnClickListener { rollDice3() }
     }
 
     private fun rollDice(){
         val resultText: TextView = findViewById(R.id.result_text)
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        val resultText3: TextView = findViewById(R.id.result_text3)
         /* resultText.text = "Dice Rolled!" //temporary string which temporary changed the text "Hello World!" -> "Dice Rolled!"
         */
         val randomInt = Random().nextInt(6) + 1
+        val randomInt2 = Random().nextInt(6) + 1
+        val randomInt3 = Random().nextInt(6) + 1
         resultText.text = randomInt.toString()
-
+        resultText2.text = randomInt2.toString()
+        resultText3.text = randomInt3.toString()
 
         //Toast is a view that shows a simple message in a small popup window.
         //A duration, for example (Toast.LENGTH_SHORT)
@@ -37,7 +45,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice2(){
         val resultText: TextView = findViewById(R.id.result_text)
-
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        val resultText3: TextView = findViewById(R.id.result_text3)
         //If text is "Hello World!" set that text to 1
         if (resultText.text == "Hello World!")
         {
@@ -55,5 +64,43 @@ class MainActivity : AppCompatActivity() {
                 resultText.text = resultInt.toString()
             }
         }
+
+        if (resultText2.text == "Hello World 2!")
+        {
+            resultText2.text = "1"
+        }
+        else
+        {
+            var resultInt2 = resultText2.text.toString().toInt()
+
+            if (resultInt2 < 6) {
+                resultInt2++
+                resultText2.text = resultInt2.toString()
+            }
+        }
+
+        if (resultText3.text == "Hello World 3!")
+        {
+            resultText3.text = "1"
+        }
+        else
+        {
+            var resultInt3 = resultText3.text.toString().toInt()
+
+            if (resultInt3 < 6) {
+                resultInt3++
+                resultText3.text = resultInt3.toString()
+            }
+        }
+    }
+
+    private fun rollDice3(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        val resultText3: TextView = findViewById(R.id.result_text3)
+
+        resultText.text = "0"
+        resultText2.text = "0"
+        resultText3.text = "0"
     }
 }
